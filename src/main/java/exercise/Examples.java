f4cff52630d2e982fa2b0e75ede823fadfe1db57
larry659 <larryfunmbi26@gmail.com>
2023-09-06 11:00:33 +0100

    resizing of array

--- src/main/java/exercise/Examples.java
+++ src/main/java/exercise/Examples.java
@@ -11,8 +11,10 @@ public class Examples {
 //        }
 //        System.out.println(sum);
         int[] myNum = {5,8,9,7,2};
+        int[] sizes = {1,0,4,5,6,0,2};
         System.out.println(Arrays.toString(reverseArray(myNum)));
         System.out.println(findMinimumValue(myNum));
+        System.out.println(Arrays.toString(movesZerosToTheEnd(sizes)));
    }
 /*Algorithm to reverse an array*/
    public static int [] reverseArray(int [] arr){
@@ -51,4 +53,34 @@ public class Examples {
        }
 return secondMax;
    }
+
+   //How to move zeroes to the end of an array
+    //sample {1,0,6,5,4,0,2}
+    //output{1,6,5,4,2,0,0}
+    public static int[] movesZerosToTheEnd(int [] arr){
+       int[] newArr = new int[arr.length];
+       int pos = arr.length - 1;
+       int frontPos = 0;
+       for(int i =0; i<arr.length; i++){
+           if(arr[i]>0){
+               newArr[frontPos]=arr[i];
+               frontPos ++;
+           }
+           else{
+               newArr[pos]=arr[i];
+               pos --;
+           }
+       }
+       return newArr;
+    }
+
+    //How to resize an array
+    public static int[] resizeArray(int [] arr, int capacity){
+       //capacity is the size you want your new array to assume
+       int [] newArray = new int[capacity];
+       for(int i=0; i< arr.length; i++){
+           newArray[i]=arr[i];
+       }
+       return newArray;
+    }
 }
